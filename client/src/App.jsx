ba753f451b892a0e4488be5ff83be3277d88cd2c
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-10-30 11:20:57 +0530

    adding client side components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Tickets from './pages/Tickets';
+import Customers from './pages/Customers';
 
 function App() {
-
   return (
-    <>
-      <h1  className='bg-yellow-200'>this is client side </h1>
-    </>
-  )
+    <div className="flex">
+      <Router>
+        <Sidebar />
+        <div className="flex-grow p-6">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/customers" element={<Customers />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
