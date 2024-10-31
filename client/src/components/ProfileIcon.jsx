4db8125925d5a68b6111d235be7bb8ec5d1c769d
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-10-31 15:09:31 +0530

    admin avatar is completed

--- client/src/components/ProfileIcon.jsx
+++ client/src/components/ProfileIcon.jsx
@@ -1,18 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contextApi/AuthContext';
+import { getCurrentUser } from '../../api/api';
 
 const ProfileIcon = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { handleLogout } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+  const { handleLogout, token } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    const fetchUser = async() => {
+        const user = await getCurrentUser(token);
+        setUser(user.data);
+    }
+    fetchUser();
+  },[])
+
   return (
     <div className="absolute right-5 top-7 inline-block text-left ">
       <button onClick={toggleMenu} className="size-12 flex justify-center items-center bg-red-300 rounded-full">
-        <span className="size-12 text-2xl rounded-full flex justify-center items-center font-semibold text-gray-800">A</span>
+        <span className="size-12 text-2xl rounded-full flex justify-center items-center font-semibold text-gray-800">{user?.avatar}</span>
       </button>
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20">
